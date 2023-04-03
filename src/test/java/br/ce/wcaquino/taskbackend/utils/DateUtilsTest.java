be534d5c16489dcc84df5cb4cfe1672121cca069
Jamesson Junior <jamesson.junior@capco.com>
2023-04-03 14:10:28 -0300

    Testes adicionados

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+  @Test
+  public void deveRetornarTrueParaDatasFuturas() {
+    LocalDate date = LocalDate.of(2030, 01, 01);
+    boolean result = DateUtils.isEqualOrFutureDate(date);
+    Assert.assertTrue(result);
+  }
+
+  @Test
+  public void deveRetornarFalseParaDatasPassadas() {
+    LocalDate date = LocalDate.of(2010, 01, 01);
+    boolean result = DateUtils.isEqualOrFutureDate(date);
+    Assert.assertFalse(result);
+  }
+
+  @Test
+  public void deveRetornarTrueParaDataAtual() {
+    LocalDate date = LocalDate.now();
+    boolean result = DateUtils.isEqualOrFutureDate(date);
+    Assert.assertTrue(result);
+  }
+
+}
